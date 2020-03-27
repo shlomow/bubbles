d14f90a49cbbee7f382da0a7f343985d5cc517e7
Shlomi Vaknin <shlomi.39sd@gmail.com>
2020-03-27 16:36:42 +0300

    fix context work dir creation

--- bubbles/utils/context.py
+++ bubbles/utils/context.py
@@ -8,6 +8,7 @@ class Context:
         if path is None:
             path = Context.work_path
         self.work_path = pathlib.Path(path)
+        self.work_path.mkdir(parents=True, exist_ok=True)
 
     def path(self, filename):
         return self.work_path / filename
